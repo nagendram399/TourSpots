87cf835de7f083e153c0897ac791e312f9415ff4
nagendram339 <nagendram399@gmail.com>
2021-07-11 19:22:16 +0530

    Start script

--- app.js
+++ app.js
@@ -133,6 +133,7 @@ app.use((err,req,res,next)=>{
     res.status(statuscode).render('error',{err});
    
 })
-app.listen(3000,()=>{
-    console.log("Server is running successfully");
+const port=process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`Server is running successfully on ${port}`);
 })
